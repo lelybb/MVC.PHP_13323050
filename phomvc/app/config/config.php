91d1fce3d99432078155eb0b625ef2f470d3f8db
lelybb <lelybutarbutar15@gmail.com>
2024-04-12 20:21:44 +0700

    Create config.php

---
+++ phomvc/app/config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('BASEURL', 'http://localhost:8080/PABI/Week%2011/PABIW10S2/Video%202/MVC_13323050/phomvc/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_NAME', 'phpmvc');
+
+?>
