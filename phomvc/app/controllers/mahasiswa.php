2dc6ac16f5f66d9782f857436a48ec89cc0244c8
lelybb <lelybutarbutar15@gmail.com>
2024-04-12 20:21:52 +0700

    Create mahasiswa.php

---
+++ phomvc/app/controllers/mahasiswa.php
@@ -0,0 +1,29 @@
+<?php
+
+class Mahasiswa extends Controller {
+    public function index(){
+        $data['judul'] = 'Daftar Mahasiswa';
+        $data['mhs'] = $this->model('Mahasiswa_model')->getAllMahasiswa();
+        $this->view('template/header', $data);
+        $this->view('mahasiswa/index', $data);
+        $this->view('template/footer');
+    }
+
+    public function detail($id){
+        $data['judul'] = 'Detail Mahasiswa';
+        $data['mhs'] = $this->model('Mahasiswa_model')->getMahasiswaByid($id);
+        $this->view('template/header', $data);
+        $this->view('mahasiswa/detail', $data);
+        $this->view('template/footer');
+    }
+
+    public function tambah (){
+        // var_dump($_POST);
+        if($this->model('Mahasiswa_model')->tambahDataMahasiswa($_POST) > 0 ){
+            header('location: ' . BASEURL . '/mahasiswa');
+            exit;
+        }
+    }
+}
+
+?>
