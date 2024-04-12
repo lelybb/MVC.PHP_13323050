ed83cb2ae0555b498fdceb9dc910f7fdf119c119
lelybb <lelybutarbutar15@gmail.com>
2024-04-12 23:15:38 +0700

    Update footer.php

--- phomvc/app/views/template/footer.php
+++ phomvc/app/views/template/footer.php
@@ -1,6 +1,17 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="http://localhost:8080/PABI/Week%2011/PABIW10S2/Video%202/MVC_13323050/phomvc/public/js/bootstrap.js"></script>
+<script src="http://localhost:8080/PABI/Week%2011/PABIW10S2/Video%202/MVC_13323050/phomvc/public/js/script.js"></script>
+<!-- Penggunaan sintaks dibawah ini karena, sebelum saya tidak menggunakan sintak ini ada erro saat ingin
+membuka console.log dari ubah  dengan warningnya "script.js:20  Uncaught TypeError: $.ajax is not a function
+    at HTMLAnchorElement.<anonymous> (script.js:20:13)
+    at HTMLAnchorElement.dispatch (jquery-3.3.1.slim.min.js:2:41964)
+    at v.handle (jquery-3.3.1.slim.min.js:2:39983)
+   $.ajax({",  Kesalahan menunjukkan bahwa fungsi $.ajax() tidak ditemukan. Ini menandakan bahwa jQuery mungkin tidak 
+    dimuat dengan benar dan agar hal itu tidak terjadi lagi akhirnya saya menggunakan  CDN yang menyediakan versi lengkap-->
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+
+
 
 </body>
 </html>      
