9648da62572fc34b7f3a5536056ce75c744c95e9
lelybb <lelybutarbutar15@gmail.com>
2024-04-12 20:22:11 +0700

    Create index.php

---
+++ phomvc/app/views/about/index.php
@@ -0,0 +1,20 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman About</title>
+</head>
+<body> -->
+<div class="container">
+    
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL ?>/img/IMG-20230901-WA0061.jpg" alt="D3TK'23" class="rounded-circle shadow">
+    <h3 style="color: red;">Halo, Kami dari <?= $data['nama']; ?>, rata-rata umur kami <?= $data['umur']; ?> tahun, kami sangat suka dengan <?= $data['pekerjaan']; ?>.
+     btw itu foto kami dengan D3TK21</h3>
+
+<!-- </body>
+</html> -->
+
+
+</div>
